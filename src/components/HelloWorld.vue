1305cf1430c637fd9d307262ed6022fc879b178d
sunyong <sunyongmofang@163.com>
2018-09-07 09:12:15 +0800

    添加数据展现功能

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,9 +9,15 @@
       <el-option v-for="ss in schemas" :key="ss.schema_name" :label="ss.schema_name" :value="ss.schema_name"></el-option>
     </el-select>
 
-    <el-select v-model="table" placeholder="请选择">
+    <el-select v-model="table" placeholder="请选择" @change="get_data()">
       <el-option v-for="tt in tables" :key="tt.name" :label="tt.name" :value="tt.name"></el-option>
     </el-select>
+
+    <div>
+      <el-table border :data="tabledata">
+        <el-table-column v-for="label in field" :key="label" :prop="label" :label="label"></el-table-column>
+      </el-table>
+    </div>
 
   </div>
 </template>
@@ -29,9 +35,26 @@ export default {
   name: 'HelloWorld',
   methods: {
     get_value: function () {
-      this.$axios({ method: 'get', url: '/prest/' + this.database + '/' + this.schema }).then(res => {
-        this.tables = res.data
-      })
+      if (this.database && this.schema) {
+        let target = [this.database, this.schema]
+        this.$axios({ method: 'get', url: '/prest/' + target.join('/') }).then(res => {
+          this.tables = res.data
+          this.table = ''
+        })
+      }
+    },
+    get_data: function () {
+      if (this.database && this.schema && this.table) {
+        let target = [this.database, this.schema, this.table]
+        this.field = []
+        this.tabledata = []
+        this.$axios({ method: 'get', url: '/prest/' + target.join('/') + '?_page=1&_page_size=10' }).then(res => {
+          this.tabledata = res.data
+          for (let i in this.tabledata[0]) {
+            this.field.push(i)
+          }
+        })
+      }
     }
   },
   mounted: function () {
@@ -49,7 +72,9 @@ export default {
       tables: [],
       database: '',
       schema: '',
-      table: ''
+      table: '',
+      tabledata: [],
+      field: []
     }
   }
 }
