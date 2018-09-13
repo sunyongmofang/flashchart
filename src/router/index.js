6e908ebd2e5784024462f116c827e8d7fbeb1bac
sunyong <sunyongmofang@163.com>
2018-09-13 20:37:17 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
