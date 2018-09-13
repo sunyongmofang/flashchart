e8c4b9feb0154ff465b803f49128c2f0f8e4b578
sunyong <sunyongmofang@163.com>
2018-09-13 22:52:52 +0800

    添加导航

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    },
-    {
-      path: '/login',
-      name: 'HelloWorld',
-      component: HelloWorld
     }
   ]
 })
