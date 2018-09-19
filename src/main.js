8e37ae2e78d749763a176edc8132606707154640
sunyong <sunyongmofang@163.com>
2018-09-19 09:26:55 +0800

    调整布局样式，重新添加导航

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import PluginAxios from '@/plugin/axios'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'normalize.css'
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+
+Vue.use(ElementUI)
+Vue.use(PluginAxios)
+Vue.component('icon', Icon)
 
 Vue.config.productionTip = false
 
--- src/pages/SqlLib/page.vue
+++ src/pages/SqlLib/page.vue
@@ -1,40 +1,65 @@
 <template>
   <el-container>
 
-    <el-aside>
-      <el-select v-model="database" placeholder="请选择database" @change="getTables()">
-        <el-option v-for="db in databases" :key="db.datname" :label="db.datname" :value="db.datname"></el-option>
-      </el-select>
+    <el-header>
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="3" disabled>消息中心</el-menu-item>
+        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+      </el-menu>
+    </el-header>
 
-      <el-select v-model="schema" placeholder="请选择schema" @change="getTables()">
-        <el-option v-for="sm in schemas" :key="sm.schema_name" :label="sm.schema_name" :value="sm.schema_name"></el-option>
-      </el-select>
+    <el-container>
 
-      <el-select v-model="table" placeholder="请选择table" @change="getData()">
-        <el-option v-for="tb in tables" :key="tb.name" :label="tb.name" :value="tb.name"></el-option>
-      </el-select>
-    </el-aside>
+      <el-aside>
+        <el-select v-model="database" placeholder="请选择database" @change="getTables">
+          <el-option v-for="db in databases" :key="db.datname" :label="db.datname" :value="db.datname"></el-option>
+        </el-select>
 
-    <el-main>
-      <FlashTable :tableData="tableData" :field="field"></FlashTable>
+        <el-select v-model="schema" placeholder="请选择schema" @change="getTables">
+          <el-option v-for="sm in schemas" :key="sm.schema_name" :label="sm.schema_name" :value="sm.schema_name"></el-option>
+        </el-select>
 
-      <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="textarea"></el-input>
+        <el-select v-model="table" placeholder="请选择table" @change="getData()">
+          <el-option v-for="tb in tables" :key="tb.name" :label="tb.name" :value="tb.name"></el-option>
+        </el-select>
+      </el-aside>
 
-      <el-button type="primary" @click="postText()">执行sql</el-button>
-    </el-main>
+      <el-main>
+        <FlashTable :tableData="tableData" :field="field"></FlashTable>
+
+        <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="textarea"></el-input>
+        <el-button type="primary" @click="postText()">执行sql</el-button>
+      </el-main>
+
+    </el-container>
+
   </el-container>
 </template>
 
 <script>
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-import PluginAxios from '@/plugin/axios'
 import FlashTable from '@/components/flash-table'
 import { mapState, mapGetters, mapActions } from 'vuex'
 import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.use(ElementUI)
-Vue.use(PluginAxios)
 
 export default {
   name: 'SqlLib',
@@ -69,6 +94,9 @@ export default {
     },
     getData: function () {
       this.setTableData({ vm: this, database: this.database, schema: this.schema, table: this.table })
+    },
+    handleSelect: function (key, keyPath) {
+      console.log(key, keyPath)
     }
   },
   mounted: function () {
