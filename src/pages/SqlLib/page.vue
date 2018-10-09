127c80957dfedbe2fff892dfb0141f984ecde98d
sunyongmofang <sunyongmofang@163.com>
2018-10-09 18:52:52 +0800

    添加更多的注释，删除无用的文件，添加了一个垃圾功能

--- src/pages/SqlLib/page.vue
+++ src/pages/SqlLib/page.vue
@@ -30,6 +30,7 @@
           </el-input>
         </div>
         <el-button type="primary" @click="postText()" class="native-margin">执行sql</el-button>
+        <el-button type="primary" @click="createSlice" class="native-margin">创建图表</el-button>
         <flash-table :tableData="datas"/>
       </el-col>
     </el-row>
@@ -38,7 +39,7 @@
 </template>
 
 <script>
-import FlashTable from '@/components/flash-table'
+import FlashTable from './components/flash-table'
 import { mapState, mapGetters, mapActions } from 'vuex'
 import 'element-ui/lib/theme-chalk/index.css'
 
@@ -58,6 +59,9 @@ export default {
       'schemaList',
       'tableList'
     ]),
+    /**
+     * @description 实时监听sql语句，并动态匹配生成变量字段
+     */
     analyText: function () {
       if (this.params.sql && this.params.sql.match(creg)) {
         return this.params.sql.match(creg).map((item) => item.replace(/"{{./g, '').replace(/}}"/g, ''))
@@ -75,10 +79,28 @@ export default {
       'analyParams',
       'execSql'
     ]),
+    /**
+     * @description 提交数据，执行sql语句
+     */
     postText: function () {
       this.params.filename = 'sqllib'
       this.execSql({ vm: this, params: this.params })
     },
+    createSlice: function () {
+      this.$prompt('请输入图表名称', '创建图表', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消'
+      }).then(({value}) => {
+        this.$message({
+          type: 'success',
+          message: '你的图表名是: ' + value
+        })
+      })
+    },
+    /**
+     * @description 接收组件数据库相关字段变得，并动态改变computed中的数据
+     * @param {String} 数据库信息变动监听
+     */
     changeParams: function (param) {
       if (this.params.database === param) {
         this.params.schema = ''
@@ -90,6 +112,7 @@ export default {
     }
   },
   mounted: function () {
+    // 初始化dataset.js中的数据
     this.updateDatabases({ vm: this })
   },
   data () {
@@ -101,6 +124,7 @@ export default {
 </script>
 
 <style scoped>
+/*自然间距*/
 .native-margin {
   margin-bottom: 20px;
 }
